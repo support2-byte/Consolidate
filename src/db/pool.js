4a60543f2fcc27c5dde6b570d7f7a9beee028a66
support2-byte <support2@royalgulfshipping.com>
2026-03-26 10:26:48 -0700

    UPDATED-CHANGES

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,16 +1,3 @@
-// import pkg from "pg";
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// const { Pool } = pkg;
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   // ssl: { rejectUnauthorized: false },
-// });
-
-// export default pool;
-
-// src/config/db.js
 import pkg from 'pg';
 const { Pool } = pkg;
 import dotenv from "dotenv";
